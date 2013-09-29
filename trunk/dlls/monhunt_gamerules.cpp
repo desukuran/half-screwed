4a6780cc82d99c260b0e2be57f1d78ea4d872156
DethKlan@gmail.com <DethKlan@gmail.com@2f155e9f-0908-2c7a-0ca5-f3e920ff9f33>
2013-09-29 04:16:47 +0000

    Monster Hunt more or less works and gives points.
    TODO: Make harder monsters give more points
    Make monsters change their AI based on the gamemode. (Like fear or even fight back, having no affiliation with the player.)

--- trunk/dlls/monhunt_gamerules.cpp
+++ trunk/dlls/monhunt_gamerules.cpp
@@ -63,63 +63,19 @@ void CMonsterplay::PlayerSpawn( CBasePlayer *pPlayer )
 	if ( addDefault )
 	{
 		pPlayer->GiveNamedItem( "weapon_fotn" );
-		pPlayer->GiveAmmo( 15, "gold", GOLDENGUN_MAX_CARRY );
+		//pPlayer->GiveAmmo( 15, "gold", GOLDENGUN_MAX_CARRY );
 			//SERVER_COMMAND("mp3 play media/jayson.mp3\n");
 	}
 }
 
 void CMonsterplay::PlayerKilled( CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor )
 {
-	CBasePlayer *peKiller = NULL;
-	CBaseEntity *ktmp = CBaseEntity::Instance( pKiller );
-	if ( pVictim->pev == pKiller )  
-	{  // killed self
-		pVictim->pev->frags -= 10;
-		char victext[1024] = "You killed yourself looking for Jason\nYou sacrificed your life for Jason\nYou lose 10 Jasons for revival costs.\n";
-		UTIL_SayText( victext, pVictim );
-		return;
-	}
-	else if ( ktmp && ktmp->IsPlayer() )
-	{
-		int FUCK = pVictim->pev->frags;
-		pKiller->frags += JasonsStolen(FUCK);
-		pVictim->pev->frags -= JasonsStolen(FUCK);
+		
+}
 
-		char victext[1024] = "You lose your Jasons to the killer.\nKILL HIM AND GET THEM BACK! ;)\n";
-		UTIL_SayText( victext, pVictim );
-
-		MESSAGE_BEGIN( MSG_ALL, gmsgScoreInfo );
-		WRITE_BYTE( ENTINDEX(pVictim->edict()) );
-		WRITE_SHORT( pVictim->pev->frags );
-		WRITE_SHORT( pVictim->m_iDeaths );
-		WRITE_SHORT( 0 );
-		WRITE_SHORT( g_pGameRules->GetTeamIndex( pVictim->m_szTeamName ) + 1 );
-		MESSAGE_END();
-
-	CBaseEntity *ep = CBaseEntity::Instance( pKiller );
-	if ( ep && ep->Classify() == CLASS_PLAYER )
-	{
-		CBasePlayer *PK = (CBasePlayer*)ep;
-
-		char kiltext[1024] = "You stole your victim's Jasons and it is now added to your score.\nNo douct hes out for blood, watch out ;)\n";
-		UTIL_SayText( kiltext, PK );
-
-		MESSAGE_BEGIN( MSG_ALL, gmsgScoreInfo );
-			WRITE_BYTE( ENTINDEX(PK->edict()) );
-			WRITE_SHORT( PK->pev->frags );
-			WRITE_SHORT( PK->m_iDeaths );
-			WRITE_SHORT( 0 );
-			WRITE_SHORT( GetTeamIndex( PK->m_szTeamName) + 1 );
-		MESSAGE_END();
-
-		// let the killer paint another decal as soon as he'd like.
-		PK->m_flNextDecalTime = gpGlobals->time;
-	}
-	else
-	{
-		// World did them in, Genuflect.
-	}
-	}
+int CMonsterplay::iKillforMonster()
+{
+	return 1; //TODO: Filter by monster for different points.
 }
 
 int CMonsterplay::JasonsStolen(int jason)
