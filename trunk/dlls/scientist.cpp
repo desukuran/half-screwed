5fcbba65f27917c3c74758790a5b9020925af280
DethKlan@gmail.com <DethKlan@gmail.com@2f155e9f-0908-2c7a-0ca5-f3e920ff9f33>
2013-10-13 18:42:44 +0000

    Daily Commit - Day 9
    scientist.cpp - Not much. Just testing healing for Monster Hunt for possibly making the scientists attack you by giving you poison.

--- trunk/dlls/scientist.cpp
+++ trunk/dlls/scientist.cpp
@@ -1086,7 +1086,10 @@ void CScientist::Heal( void )
 	if ( target.Length() > 100 )
 		return;
 
-	m_hTargetEnt->TakeHealth( gSkillData.scientistHeal, DMG_GENERIC );
+	if (g_pGameRules->IsMonster())
+		m_hTargetEnt->TakeHealth( -gSkillData.scientistHeal, DMG_POISON );
+	else
+		m_hTargetEnt->TakeHealth( gSkillData.scientistHeal, DMG_GENERIC );
 	// Don't heal again for 1 minute
 	m_healTime = gpGlobals->time + 60;
 }
